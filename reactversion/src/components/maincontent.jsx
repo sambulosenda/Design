b3174b63575296a681c31760789b6c3fe097bb36
Sambulo Senda <sambulosendas@gmail.com>
2018-12-25 01:42:43 +0000

    react main content

--- reactversion/src/components/maincontent.jsx
+++ reactversion/src/components/maincontent.jsx
@@ -20,48 +20,49 @@ class maincontent extends Component {
           <p>Unde omins</p>
           <p>voluptatem(devrnem)</p>
         </div>
-        <div class="column">
+        <div className="column">
           <p>Perspiciatis Unde</p>
           <p>Architecto beatae vitae</p>
           <p>Perspiciatis Unde</p>
           <p>Architecto beatae vitae</p>
         </div>
-        <div class="column">
-          <div class="low" <p>architecto beatae vitae</p>
+        <div className="column">
+          <div className="low">
+         ><p>architecto beatae vitae</p>
           </div>
         </div>
       </div>
 
-      <div class="row">
-        <div class="column">
+      <div className="row">
+        <div className="column">
 
           <p>Totam</p>
           <p>Iste Natus</p>
           <p>Omnis Iste Natus Erro</p>
           <p>Explicabo</p>
         </div>
-        <div class="column">
+        <div className="column">
           <p>5.0</p>
           <p>Omnis iste natus</p>
           <p>PAccusantium deloremque laudant</p>
           <p>Accusantium dolor</p>
         </div>
-        <div class="column">
+        <div className="column">
 
 
         </div>
       </div>
-      <div class="buttomcontrols">
-        <div class="row buttomborder">
+      <div className="buttomcontrols">
+        <div className="row buttomborder">
           <a href="index.html" class="brown">Perspiciatis Unde</a>
           <a href="index.htm" class="highlighted"> <span class="underline">Accusantium deloremque laudant</span></a>
           <a href="index.htm" class="grey"> Voluptatem(duvrnmen)</a>
         </div>
         <hr>
-        <div class="row ">
+        </hr>
+        <div className="row ">
           <a href="" class="brown">Iste Natus</a>
-          <!--<div class="dropdown">
- </div>-->
+        
           <select class="round">
             <option value="Accusantium deloremque laudant">Accusantium deloremque laudant</option>
             <option value="Architecto beatae vitae">Architecto beatae vitae</option>
